82845acb1669596b30ed540b906f0d5920135088
unknown <daheimao003@gmail.com>
2020-05-28 10:54:47 -0500

    Add manager register and login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,25 +6,109 @@ using System.Web.Mvc;
 
 namespace EnterpriseTask1.Controllers
 {
+    //[ADKZAuthority]
     public class HomeController : Controller
     {
+        Context db = new Context();
+        [AllowAnonymous]
         public ActionResult Index()
         {
+            if (Session["LoginName"] != null)
+            {
+                string name = Session["LoginName"].ToString();
+
+                ViewBag.name = name;
+                var id = db.Managers.Where(m => m.Email == name).FirstOrDefault().Id;
+                ViewBag.Projects = db.Projects.Where(p => p.Manager.Email == name).ToList();
+                var staff = db.Staffs.Where(s => s.ManagerId == id);
+
+                var projects = db.Projects.Where(p => p.Manager.Email == name);
+                foreach (var p in projects.ToList())
+                {
+                    foreach (var task in p.Tasks.ToList())
+                    {
+                        TimeSpan t = (DateTime.Now - task.Deadline);
+                        if (t.TotalDays > 0)
+                        {
+                            var notificationList = db.Notifications.Where(nt => nt.Staff.ManagerId == id);
+                            if (!notificationList.Any(n => n.Task == task.Id))
+                            {
+                                var staffName = db.Staffs.Find(task.StaffId).Name;
+                                var n = new Notification();
+                                n.Title = $"Warning...";
+                                n.Content = $"{staffName} didn't finish task on time...,Fire him.";
+                                n.StaffId = task.StaffId;
+                                n.IsChecked = false;
+                                n.IsBeyondDeadline = true;
+                                n.Task = task.Id;
+                                db.Notifications.Add(n);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+                }
+
+                ViewBag.notification = db.Notifications.Where(n => n.Staff.ManagerId == id);
+                var x = db.Staffs.Where(s => s.ManagerId == id);
+
+                return View(x);
+            }
+            else
+            {
+                return Redirect("/Home/ManagerLogin");
+            }
+            /* ViewBag.name = Session["LoginName"].ToString() != null ? Session["LoginName"].ToString() : "bad";
+            return View();*/
+        }
+        [AllowAnonymous]
+        public ActionResult ManagerRegister()
+        {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult ManagerRegister(ManagerModel manager)
         {
-            ViewBag.Message = "Your application description page.";
+            if (!ModelState.IsValid)
+            {
+                return View(new ManagerModel());
+            }
 
+            Session["LoginName"] = manager.Email;
+            Manager x = new Manager();
+            x.Email = manager.Email;
+            x.Name = manager.Name;
+            x.Password = manager.Password;
+            db.Managers.Add(x);
+            int result = db.SaveChanges();
+            if (result > 0)
+            {
+                return Redirect(Url.Action("Index"));
+            }
+            else
+            {
+                return View(new ManagerModel());
+            }
+        }
+        [AllowAnonymous]
+        public ActionResult ManagerLogin()
+        {
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult ManagerLogin(ManagerModel model)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var x = db.Managers.Where(m => m.Email == model.Email && m.Password == model.Password && m.Name == model.Name).FirstOrDefault();
+            if (x != null)
+            {
+                Session["LoginName"] = x.Email;
+                return Redirect(Url.Action("Index", "Home"));
+            }
+            else
+            {
+                ViewBag.message = "Didn't found";
+                return View();
+            }
         }
     }
 }
