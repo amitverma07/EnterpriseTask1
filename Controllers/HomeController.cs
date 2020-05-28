719b2d1cbd2b157c89a2a1a76400751f932da8cb
unknown <daheimao003@gmail.com>
2020-05-28 11:01:12 -0500

    Add Name

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace EnterpriseTask1.Controllers
+namespace ADKZProject.Controllers
 {
     //[ADKZAuthority]
     public class HomeController : Controller
