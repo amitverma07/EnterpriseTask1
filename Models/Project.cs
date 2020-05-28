333e274fca757677c57f7263f7722e57bc9dccdc
test <test@example.comgit>
2020-05-27 20:16:03 -0500

    update table structure

--- Models/Project.cs
+++ Models/Project.cs
@@ -10,6 +10,33 @@ namespace ADKZProject.Models
 {
     public class Project
     {
-               
+        public Project()
+        {
+            this.Tasks = new HashSet<Task>();
+        }
+
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ManagerId { get; set; }
+        public virtual Manager Manager { get; set; }
+        [Required]
+        public decimal Budget { get; set; }
+        public Nullable<decimal> RealBudget { get; set; }
+        public DateTime CreateTime { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime Deadline { get; set; }
+        public Nullable<DateTime> FinishedTime { get; set; }
+        [Required]
+        public bool IsFinished { get; set; } = false;
+        [Required]
+        [MaxLength(100)]
+        public string ProjectTitle { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string ProjectContent { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+        [Required]
+        public int Priority { get; set; } = 1;
+
     }
 }
