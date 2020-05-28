ed8900ad467f61c074a8897f32733fec669db90e
Zifan Liu <zfan0310@gmail.com>
2020-05-28 16:03:56 -0500

    update authority

--- Models/ADKZAuthority.cs
+++ Models/ADKZAuthority.cs
@@ -6,8 +6,21 @@ using System.Web.Mvc;
 
 namespace ADKZProject.Models
 {
-    public class ADKZAuthority: AuthorizeAttribute
+    public class ADKZAuthority : AuthorizeAttribute
     {
-        // init
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.ActionDescriptor.IsDefined(typeof(AllowAnonymousAttribute), true)
+                || filterContext.ActionDescriptor.ControllerDescriptor.IsDefined(typeof(AllowAnonymousAttribute), true))
+            {
+                return;
+            }
+            if (filterContext.HttpContext.Session["LoginName"] == null || filterContext.HttpContext.Session["LoginName"].ToString() == "")
+            {
+                HttpContext.Current.Response.Write("Inadmissibility");
+                HttpContext.Current.Response.End();
+                filterContext.Result = new RedirectResult("/Home/ManagerRegister");
+            }
+        }
     }
 }
