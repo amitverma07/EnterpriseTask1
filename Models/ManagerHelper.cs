a74c4f9061abc2542a6f1b541c8f196bafde7d75
AMIT VERMA <55926153+amitverma07@users.noreply.github.com>
2020-05-31 17:45:16 -0500

    Update ManagerHelper.cs

--- Models/ManagerHelper.cs
+++ Models/ManagerHelper.cs
@@ -7,6 +7,32 @@ namespace ADKZProject.Models
 {
     public static class ManagerHelper
     {
-       
+        public static bool DeleteStaff(Guid Staffid, Context db)
+        {
+            var x = db.Staffs.Find(Staffid);
+            var tasks = db.Tasks.Where(t => t.StaffId == Staffid).ToList();
+            var notifications = db.Notifications.Where(n => n.StaffId == Staffid).ToList();
+
+            foreach (var item in tasks)
+            {
+                db.Tasks.Remove(item);
+                db.SaveChanges();
+            }
+            foreach (var item in notifications)
+            {
+                db.Notifications.Remove(item);
+                db.SaveChanges();
+            }
+            db.Staffs.Remove(x);
+            int r = db.SaveChanges();
+            if (r > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
