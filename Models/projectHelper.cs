87d3ccbc9164bace9502f806ff9af2f1c4860ace
test <test@example.comgit>
2020-05-30 20:45:10 -0500

    finish the projectHelper

--- Models/projectHelper.cs
+++ Models/projectHelper.cs
@@ -7,6 +7,45 @@ namespace ADKZProject.Models
 {
     public static class projectHelper
     {
+        public static bool AddProject(Guid id, ProjectModel  p,Context c)
+        {
+            var project = new Project();
+            project.ManagerId = id;
+            project.Budget = p.Budget;
+            project.CreateTime = DateTime.Now;
+            project.Deadline = p.Deadline;
+            project.ProjectTitle = p.ProjectTitle;
+            project.IsFinished = false;
+            project.Priority = 1;
+            project.ProjectContent = p.ProjectContent;
+            c.Projects.Add(project);
+            int r=c.SaveChanges();
+            if (r > 0)
+            {
+                return true;
+            }
+            return false;
+        }
 
+        public static bool DeleteProject(Guid ProjectId, Context c)
+        {
+            var x = c.Projects.Find(ProjectId);
+            if (x != null)
+            {
+                foreach (var item in x.Tasks.ToList())
+                {
+                    c.Tasks.Remove(item);
+                    c.SaveChanges();
+                }
+                c.Projects.Remove(x);
+                c.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
     }
 }
